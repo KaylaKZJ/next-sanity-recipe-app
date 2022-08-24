e180b5cdc30b2f8470ea829a8679143bace6e4cb
KaylaKZJ <kaylajoubert33@gmail.com>
2022-08-24 08:39:43 +0200

    Adds styling and portable text

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Image from 'next/image';
 import { SanityClient, usePreviewSubscription } from 'next-sanity';
-import { sanityClient, urlFor, PortableText } from '../../lib/sanity';
+import { sanityClient, urlFor } from '../../lib/sanity';
+import { PortableText } from '@portabletext/react';
 
 const Recipe = ({ data }) => {
   const { recipe } = data;
   console.log(recipe.instructions);
   return (
-    <article>
+    <article className='recipe'>
       <h1>{recipe.name}</h1>
-      <main>
+      <main className='content'>
         <div className='image-wrapper'>
           <Image
             alt={recipe.name}
@@ -20,39 +21,39 @@ const Recipe = ({ data }) => {
             src={urlFor(recipe?.mainImage).url()}
           />
         </div>
-        <div>
-          <ul>
+        <div className='breakdown'>
+          <ul className='ingredients'>
             {recipe.ingredient?.map((ingredient) => (
-              <li key={ingredient._key}>
+              <li className='ingredient' key={ingredient._key}>
                 {ingredient?.wholeNumber}
-                {ingredient?.fraction} {ingredient?.unit}
-                {/* <br /> */}
+                {ingredient?.fraction} {ingredient?.unit}{' '}
                 {ingredient?.ingredient.name}
                 {ingredient?.wholeNumber > 1 ? "'s" : ''}
               </li>
             ))}
           </ul>
-          <h3>Instructions:</h3>
-          <PortableText
-            value={[recipe.instructions]}
-            components={{
-              block: {
-                // Customize block types with ease
-                h1: ({ children }) => <h1 className='text-2xl'>{children}</h1>,
-
-                // Same applies to custom styles
-                customHeading: ({ children }) => (
-                  <h2 className='text-lg text-primary text-purple-700'>
-                    {children}
-                  </h2>
-                ),
-              },
-            }}
-          />
+          <div className='instructions'>
+            <h3>Instructions:</h3>
+            <PortableText value={recipe.instructions} components={components} />
+          </div>
         </div>
       </main>
     </article>
   );
+};
+
+const components = {
+  listItem: {
+    // Ex. 1: customizing common list types
+    bullet: ({ children }) => (
+      <li className='instruction' style={{ listStyleType: 'none' }}>
+        {children}
+      </li>
+    ),
+
+    // Ex. 2: rendering custom list items
+    // checkmarks: ({ children }) => <li> {children}</li>,
+  },
 };
 
 const recipeQuery = `*[_type == "recipe" && slug.current == $slug][0]{
